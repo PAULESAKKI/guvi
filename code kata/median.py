9a430c8a1b95f0bc061762de7cca7b511e62a6be
PAULESAKKI <51704403+PAULESAKKI@users.noreply.github.com>
2019-06-14 16:31:26 +0530

    Update median.py

--- code kata/median.py
+++ code kata/median.py
@@ -1,5 +1,3 @@
-a = [int(x) for x in input().split()]
-p,q,r,s,t=a
-w=a.sort()
-median=a[2]
-print("Median is : %i" %median)
+import statistics
+c=list(map(int,input().split()))
+print(statistics.median(c))

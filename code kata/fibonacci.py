14277cc29f451ba594cb10a5e479362eecd49a0c
PAULESAKKI <51704403+PAULESAKKI@users.noreply.github.com>
2019-06-15 07:20:10 -0700

    Update fibonacci.py

--- code kata/fibonacci.py
+++ code kata/fibonacci.py
@@ -1,12 +1,12 @@
 t = int(input())
  
-f= 0
+f= 1
 s = 1
-print(f, ",", s, end=", ")
+print(f,s)
  
 for i in range(2, t):
-	next = f + s
-	print(next, end=", ")
+	next = f + s\n
+	print(next)
  
 	f = s
 	s = next

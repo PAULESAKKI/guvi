ea3f07c08e700861e5660d40b7acc1f3ed5ec7b2
PAULESAKKI <51704403+PAULESAKKI@users.noreply.github.com>
2019-07-06 05:44:36 -0700

    Update num1.py

--- code kata/num1.py
+++ code kata/num1.py
@@ -1,3 +1,3 @@
-s=int(input())
-t=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
-print(t[s])
+s1=int(input())
+t1=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
+print(t1[s1])
